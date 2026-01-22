8f45cf1b3890eee96f96e8c61da0bf18614e7eba
quan2407 <quan24072003@gmail.com>
2026-01-22 08:10:47 +0700

    btvnsession07

---
+++ btvnsession07/btvngioi2/src/Book.java
@@ -0,0 +1,39 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Book {
+   private String title;
+   private String author;
+   private double price;
+   public Book(String title, String author, double price) {
+       this.title = title;
+       this.author = author;
+       this.price = price;
+   }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String printInfo() {
+       return "Book{" + "title=" + title + ", author=" + author + ", price=" + price + '}';
+    }
+}
